75839ccbcdda18ca33aa65b026adc8086346dd67
albumsfromkyle <koolkyle26@gmail.com>
2024-07-29 18:33:19 -0500

    Updated year-list navbar to scroll and enable/disable

--- js/main.js
+++ js/main.js
@@ -1,37 +1,51 @@
-// Dummy print function to display a string on the website itself
-function print(str) {
-    var printer = document.getElementsByClassName("printer")[0];
-    printer.innerHTML = str;
+const OLDEST_YEAR = 2019
+const CURRENT_YEAR = 2024
+const NUM_YEARS = 5
+
+
+/***************
+**** HELPER ****
+***************/
+
+// Check if a given file (mostly used to check for CSV files) exists
+function checkFileExists(filename) {
+    return fetch(filename, { method: 'HEAD' })
+        .then(response => response.ok)
+        .catch(() => false);
 }
 
 
-// Detects when one of the "Year-Menu" elements is clicked on
-document.getElementById("year-menu").addEventListener("click", function(event) {
-    print(event.target.innerHTML);
-
-    // Clear the current table
-    document.getElementById("album-table-body").innerHTML = "";
-
-    // TODO: If this year does not exist, print a notice
-
-    // Load in the new CSV and display the new data
-    var year = event.target.innerHTML;
-    d3.csv("csv/" + year + ".csv").then(function(data) {
-        displayData(data);
-    });
-
-    // TODO: When an "◀" or "▶" is clicked on, shift the year list
-    // TODO: Make the only years that show be the years I have reviews for
-    // TODO: Make the "◀" or "▶" buttons gray out and not work when there are no more years to scroll to
-});
-
-
-// Loads newest CSV by default
+/********************
+**** CSV LOADING ****
+********************/
+// Loads newest CSV by on startup
 d3.csv("csv/2024.csv").then(function(data) {
     displayData(data);
 });
 
 
+// Detects when one of the "Year-Menu" elements is clicked on
+document.getElementById("year-list").addEventListener("click", async function(event) {
+    var year = event.target.innerHTML;
+
+    // TODO: If this year does not exist, do nothing (maybe print banner?)
+    var filename = "csv/" + year + ".csv";
+    const exists = await checkFileExists(filename);
+    if (!exists) {
+        return;
+    }
+
+    // Clear the current table
+    document.getElementById("album-table-body").innerHTML = "";
+
+    // Load in the new CSV and display the new data
+    d3.csv("csv/" + year + ".csv").then(function(data) {
+        displayData(data);
+    });
+});
+
+
+// Loads passed in CSV data into the album list table 
 function displayData(data) {
     const table = document.getElementById("album-table-body");
     
@@ -46,12 +60,85 @@ function displayData(data) {
         rating = new_row.insertCell(-1);
 
         album_art.innerHTML = "-";
-        artist.innerHTML = csv_row["Artist"];
-        album.innerHTML = csv_row["Album"];
-        genre.innerHTML = csv_row["Genre"];
-        favorite_songs.innerHTML = csv_row["Favorite Songs"];
-        rating.innerHTML = csv_row["Rating"];
+        artist.innerHTML = csv_row["Artist"] ? csv_row["Artist"] : "-";
+        album.innerHTML = csv_row["Album"] ? csv_row["Album"] : "-";
+        genre.innerHTML = csv_row["Genre"] ? csv_row["Genre"] : "-";
+        favorite_songs.innerHTML = csv_row["Favorite Songs"] ? csv_row["Favorite Songs"] : "-";
+        rating.innerHTML = csv_row["Rating"] ? csv_row["Rating"] : "-";
     });
 
     // TODO: Add row highlighting depending on rating
 }
+
+
+/**************************
+**** YEAR LIST BUTTONS ****
+**************************/
+// Grays out all buttons of years without corresponding CSV files
+async function grayOutMissingYears() {
+    for (let i = 1; i <= NUM_YEARS; i++) {
+        year = document.getElementById("year" + i).innerHTML;
+
+        var filename = "csv/" + year + ".csv";
+        const exists = await checkFileExists(filename);
+        
+        // Disable/enable each button depending on if the CSV file exists
+        if (exists) {
+            document.getElementById("year" + i).disabled = false;
+        }
+        else {
+            document.getElementById("year" + i).disabled = true;
+        }
+    }
+}
+
+
+// Shift the years shown by the year-list
+document.getElementById("year-increase").onclick = function() {
+    // Don't let the user go into the future
+    if (document.getElementById("year5").innerHTML >= CURRENT_YEAR) {
+        return
+    }
+
+    // Increase all year numbers
+    for (let i = 1; i <= NUM_YEARS; i++) {
+        year_element = document.getElementById("year" + i);
+        document.getElementById("year" + i).innerHTML = parseInt(year_element.innerHTML) + NUM_YEARS
+    }
+
+    // Gray out all years without a CSV
+    grayOutMissingYears();
+
+    // If the user is at the end, gray out the button
+    document.getElementById("year-increase").disabled = false;
+    document.getElementById("year-decrease").disabled = false;
+    if (document.getElementById("year5").innerHTML >= CURRENT_YEAR) {
+        document.getElementById("year-increase").disabled = true;
+    }
+};
+
+
+// Shift the years shown by the year-list
+document.getElementById("year-decrease").onclick = function() {
+    // Don't let the user go before 2019
+    if (document.getElementById("year1").innerHTML <= OLDEST_YEAR) {
+        return
+    }
+
+    // Decrease all year numbers
+    for (let i = 1; i <= NUM_YEARS; i++) {
+        year_element = document.getElementById("year" + i);
+        document.getElementById("year" + i).innerHTML = parseInt(year_element.innerHTML) - NUM_YEARS
+    }
+
+    // Gray out all years without a CSV
+    grayOutMissingYears();
+
+    // TODO: If the user is at the end, gray out the button
+    document.getElementById("year-increase").disabled = false;
+    document.getElementById("year-decrease").disabled = false;
+    if (document.getElementById("year1").innerHTML <= OLDEST_YEAR) {
+        document.getElementById("year-decrease").disabled = true;
+    }
+};
+
--- todo.md
+++ todo.md
@@ -1,38 +1,50 @@
+# DESIGN THOUGHTS
+* Two paths to go down:
+    1. It could all be one one "webpage" where I am just loading different CSV files each time
+    2. Each year/list has it's own webpage that I redirect to
+
 # GENERAL
 * Allow the user to search for an album/artist
     - Prio: Low
     - Diff: High?
+* Allow the user to search by genre
+    - Prio: Low
+    - Diff: Medium
+* Add links to my end-of-year songs and albums playlists
+    - Prio: Low
+    - Diff: Low
+* Clean up older album year Excel sheets
+    - Prio: Extremely High
+    - Diff: High
+* Maybe add "Grid Layout" in addition to "Table Layout," which will be a grid of album arts with the artist, album name, genre, and ranking beneath (no favorite songs)
 
 # NAV BAR
 * Add ability to scroll through years in the year-selector bar
     - Prio: Extremely High
     - Diff: Medium?
+* Either remove years without rankings, OR make those years "grayed out"
+    - Prio: High
+    - Diff: Medium
+* Make the selected year, and selected list, stand out
+    - Prio: High
+    - Diff: Low
 
 # TABLE
 * Make the column sizes resizeable when clicked on by the user
     - Prio: High
-    - Diff: Medium?
-* Allow the user to sort the table by the column (alphabetically, by date, by rating)
-    - Prio: Low
-    - Diff: Medium?
+    - Diff: High
+* Allow the user to sort the table by the column (alphabetically, genre, date, rating)
+    - Prio: Medium
+    - Diff: Medium
 * Adjust default table column sizes to fit better
     - Prio: High
     - Diff: Low
-* If a year does not exist, print a notice
-    - Prio: High
-    - Diff: Low
-* When an "◀" or "▶" is clicked on, shift the year list
-    - Prio: Extremely High
-    - Diff: Medium
-* Make the only years that show be the years I have reviews for
-    - Prio: High
-    - Diff: Medium
-* Make the "◀" or "▶" buttons gray out and not work when there are no more years to scroll to
-    - Prio: High
-    - Diff: Medium
 * Add row highlighting depending on rating
     - Prio: Extremely High
     - Diff: Low
 * Add default if there is no rating (or other value)
     - Prio: Extremely High
+    - Diff: Low
+* Add hidden ranking value that determines the PRECISE ranking of each album (or at least the top ones)
+    - Prio: High
     - Diff: Low
